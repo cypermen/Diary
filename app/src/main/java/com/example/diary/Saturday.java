8abe00577945bbf764027ffe0601da33f88c6654
cypermen <andreybabich2012@yandex.ru>
2019-12-02 03:27:52 +0300

    0.2

--- app/src/main/java/com/example/diary/Saturday.java
+++ app/src/main/java/com/example/diary/Saturday.java
@@ -1,13 +1,30 @@
 package com.example.diary;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Saturday extends AppCompatActivity {
+
+    String subject1;
+    String subject2;
+    String subject3;
+    String subject4;
+    String subject5;
+    String subject6;
+    Button change;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.saturday);
+        subject1 = getIntent().getStringExtra("subject1");
+        subject2 = getIntent().getStringExtra("subject2");
+        subject3 = getIntent().getStringExtra("subject3");
+        subject4 = getIntent().getStringExtra("subject4");
+        subject5 = getIntent().getStringExtra("subject5");
+        subject6 = getIntent().getStringExtra("subject6");
+        change = (Button) findViewById(R.id.button);
     }
 }
---
+++ app/src/main/java/com/example/diary/Saturday_set.java
@@ -0,0 +1,15 @@
+package com.example.diary;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Saturday_set extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
